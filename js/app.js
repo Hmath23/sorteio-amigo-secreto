fa8df27426cf9dbfee6b0b44dda4e3061750c0ea
Matheus Henrique Caraça da Silva <hmatheus23@hotmail.com>
2024-05-11 17:06:10 -0300

    Sorteio amigo secreto usando JS

---
+++ js/app.js
@@ -0,0 +1,57 @@
+const listaAmigos = [];
+
+function obterNumeroAleatorio(final, inicio) {
+    return Math.floor(Math.random() * (final - inicio + 1)) + inicio;
+}
+
+function adicionar() {
+    let nomeAmigo = document.getElementById('nome-amigo').value;
+
+    if (!nomeAmigo || nomeAmigo.trim() === "") {
+        alert("Preencha um nome válido.");
+        return;
+    }
+    
+    if (listaAmigos.includes(nomeAmigo)) {
+        alert('O nome já está na lista, coloque um sobrenome para diferenciar');
+        return;
+    } else{
+        listaAmigos.push(nomeAmigo);
+    }
+    
+    document.getElementById('lista-amigos').textContent = listaAmigos;
+    document.getElementById('nome-amigo').value = '';
+}
+
+function sortear() {
+    embaralhar(listaAmigos);
+    
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for(let i=0; i < listaAmigos.length ; i++){
+        if (i === (listaAmigos.length-1)){
+            sorteio.innerHTML += `${listaAmigos[i]} => ${listaAmigos[0]} <br>`;
+        }else{
+        sorteio.innerHTML += `${listaAmigos[i]} => ${listaAmigos[i+1]} <br>`;
+    }
+    }
+}
+
+function embaralhar(lista) {
+    let indice = lista.length
+    
+    while(indice) {
+        // atenção para o pós-incremento indice-- 
+        const indiceAleatorio = Math.floor(Math.random() * indice--);
+        [lista[indice], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice]];
+    }
+}
+
+function reiniciar() {
+    listaAmigos.splice(0,listaAmigos.length-1);
+    listaAmigos.splice(0,listaAmigos.length-1);
+    document.getElementById('lista-amigos').textContent = '';
+    document.getElementById('nome-amigo').value = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
